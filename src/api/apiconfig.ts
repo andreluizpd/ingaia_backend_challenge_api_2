beb6ddd6406d560c92c5a2693ab66481d82dd22d
Andre Luiz Pastore Dambros <andreluizpd@gmail.com>
2021-05-14 16:54:30 -0300

    Feat: Added travis

--- src/api/apiconfig.ts
+++ src/api/apiconfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ingaia-api-1.herokuapp.com/api/v1/pricesPerSquareMeters',
+  baseURL: 'https://ingaia-api-1.herokuapp.com/api/v1/',
   // baseURL: 'http://127.0.0.1:7000/api/v1/',
 });
 
